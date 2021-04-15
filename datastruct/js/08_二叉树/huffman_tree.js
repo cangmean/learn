b0e8289713f92fb4e1d8a4539fcc35441b99bd46
cangmean <cangmean@outlook.com>
2021-04-15 20:19:10 +0800

    添加最短路径查询

--- datastruct/js/08_二叉树/huffman_tree.js
+++ datastruct/js/08_二叉树/huffman_tree.js
@@ -7,120 +7,121 @@
 
 
 class _Node {
-  constructor(data = null, left = null, right = null) {
-    this.data = data
-    this.left = left
-    this.right = right
-  }
+    constructor(data = null, left = null, right = null) {
+        this.data = data
+        this.left = left
+        this.right = right
+    }
 
-  valueOf() {
-    return this.data
-  }
+    valueOf() {
+        return this.data
+    }
 }
 
 // 工厂函数
 const Node = (data, left = null, right = null) => {
-  return new _Node(data, left, right)
+    return new _Node(data, left, right)
 }
 class PriorityQue {
-  constructor(heap = []) {
-    this.heap = heap
-    this.build()
-  }
-
-  getSize() {
-    return this.heap.length
-  }
-
-  upward(i) {
-    // 向上筛选
-    // i 是数组下标, 符合条件替换父子节点
-    while (this._parent(i) >= 0 && i > 0) {
-      if (this.heap[this._parent(i)] > this.heap[i]) {
-        let tmp = this.heap[i]
-        this.heap[i] = this.heap[this._parent(i)]
-        this.heap[this._parent(i)] = tmp
-      }
-      i = this._parent(i)
+    constructor(heap = []) {
+        this.heap = heap
+        this.build()
     }
-  }
 
-  downward(i) {
-    // 向下筛选， 获取子节点中最小值，替换父节点
-    while (i * 2 <= this.getSize()) {
-      let idx = this.minChild(i)
-
-      if (this.heap[i] > this.heap[idx]) {
-        let tmp = this.heap[idx]
-        this.heap[idx] = this.heap[i]
-        this.heap[i] = tmp
-      }
-      i = idx > 0 ? idx : 1
+    getSize() {
+        return this.heap.length
     }
-  }
 
-  minChild(i) {
-    // 如果没有右子节点， 返回左子节点
-    let left_idx = i * 2
-    let right_idx = i * 2 + 1
-    if (right_idx > this.getSize()) {
-      return left_idx
-    } else if (this.heap[left_idx] < this.heap[right_idx]) {
-      return left_idx
-    } else {
-      return right_idx
+    upward(i) {
+        // 向上筛选
+        // i 是数组下标, 符合条件替换父子节点
+        while (this._parent(i) >= 0 && i > 0) {
+            if (this.heap[this._parent(i)] > this.heap[i]) {
+                let tmp = this.heap[i]
+                this.heap[i] = this.heap[this._parent(i)]
+                this.heap[this._parent(i)] = tmp
+            }
+            i = this._parent(i)
+        }
     }
-  }
 
-  _parent(i) {
-    // 父节点下标
-    return Math.floor(i / 2)
-  }
+    downward(i) {
+        // 向下筛选， 获取子节点中最小值，替换父节点
+        while (i * 2 <= this.getSize()) {
+            let idx = this.minChild(i)
 
-  enqueue(e) {
-    // 压入堆中
-    this.heap.push(e)
-    this.upward(this.getSize())
-  }
-
-  dequeue() {
-    // 弹出堆顶
-    let e = this.heap[0]
-    this.heap[0] = this.heap[this.getSize() - 1]
-    this.heap.pop()
-    this.downward(0)
-    return e
-  }
-
-  build() {
-    let i = this.getSize()
-    while (i > 0) {
-      this.upward(i)
-      i -= 1
+            if (this.heap[i] > this.heap[idx]) {
+                let tmp = this.heap[idx]
+                this.heap[idx] = this.heap[i]
+                this.heap[i] = tmp
+            }
+            i = idx > 0 ? idx : 1
+        }
     }
-  }
+
+    minChild(i) {
+        // 如果没有右子节点， 返回左子节点
+        let left_idx = i * 2
+        let right_idx = i * 2 + 1
+        if (right_idx > this.getSize()) {
+            return left_idx
+        } else if (this.heap[left_idx] < this.heap[right_idx]) {
+            return left_idx
+        } else {
+            return right_idx
+        }
+    }
+
+    _parent(i) {
+        // 父节点下标
+        return Math.floor(i / 2)
+    }
+
+    enqueue(e) {
+        // 压入堆中
+        this.heap.push(e)
+        this.upward(this.getSize())
+    }
+
+    dequeue() {
+        // 弹出堆顶
+        let e = this.heap[0]
+        this.heap[0] = this.heap[this.getSize() - 1]
+        this.heap.pop()
+        this.downward(0)
+        return e
+    }
+
+    build() {
+        let i = this.getSize()
+        while (i > 0) {
+            this.upward(i)
+            i -= 1
+        }
+    }
 }
 
 
 class Huffman {
-  constructor(list) {
-    this.tree = this.build(list)
-  }
-
-  build(list) {
-    let nodes = list.map(item => Node(item))
-    let que = new PriorityQue(nodes)
-    while (que.getSize() > 1) {
-      // 最小的节点出堆
-      let t1 = que.dequeue()
-      let t2 = que.dequeue()
-      console.log('Get Node：', t1.data, t2.data)
-
-      // 构成新的节点压入
-      que.enqueue(Node(t1.data + t2.data, t1, t2))
+    constructor(list) {
+        this.tree = this.build(list)
     }
-    return que.dequeue()
-  }
+
+    build(list) {
+        let nodes = list.map(item => Node(item))
+        let que = new PriorityQue(nodes)
+
+        while (que.getSize() > 1) {
+            // 最小的节点出堆
+            let t1 = que.dequeue()
+            let t2 = que.dequeue()
+            console.log('Get Node：', t1.data, t2.data, 'value: ', t1.data + t2.data)
+
+            // 构成新的节点压入
+            que.enqueue(Node(t1.data + t2.data, t1, t2))
+        }
+        return que.dequeue()
+    }
 
 }
 
---
+++ datastruct/js/09_图/Dijkstra.js
@@ -0,0 +1,63 @@
+/**
+ * 单源点最短路径算法
+ */
+
+
+function Dijkstra(matrix, start) {
+    let number = matrix.length
+    let s = new Array(number).fill(0) // 集合u表示是加入的顶点集合
+    let distance = new Array(number).fill(Infinity)
+
+    console.log(matrix)
+
+    let vertexs = ['A', 'B', 'C', 'D', 'E']
+
+    // ['A', 'B', 'C', 'D', 'E']
+    // 下标为到达的顶点, 比如 distance[3] 中下标表示 从start开始到D， 而值表示路径的权值。
+    distance[start] = 0
+    s[start] = 1 // 加入到集合
+
+    console.log(distance, s, '\n')
+
+
+    for (let i = 0; i < number; i++) {
+        // 到达的顶点
+
+        console.log('开始顶点: ', vertexs[i], ' index: ', i)
+        let k = 0
+        let minWeight = Infinity
+        for (let j = 0; j < number; j++) {
+            // 未加入到集合
+            if (s[j] === 0 && (distance[j] < minWeight)) {
+                k = j
+                minWeight = distance[j]
+            }
+        }
+
+        s[k] = 1
+        console.log('放入已知集合: ', vertexs[k], s)
+
+        for (let j = 0; j < number; j++) {
+            if (s[j] === 0 && distance[k] + matrix[k][j] < distance[j]) {
+                distance[j] = distance[k] + matrix[k][j]
+                console.log(`${vertexs[k]} to ${vertexs[j]} ---> ${distance[j]}`)
+            }
+        }
+        console.log('distance: ', distance)
+        console.log('\n')
+    }
+
+}
+
+function main() {
+    let matrix = [
+        [0, 4, Infinity, 2, Infinity],
+        [4, 0, 4, 1, Infinity],
+        [Infinity, 4, 0, 1, 3],
+        [2, 1, 1, 0, 7],
+        [Infinity, Infinity, 3, 7, 0]
+    ]
+    Dijkstra(matrix, 0)
+}
+
+main()
